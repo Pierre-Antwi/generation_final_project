131666ea52299f85947b00061da61b36900e762e
Siedah-Rah92 <130437292+Siedah-Rah92@users.noreply.github.com>
2023-05-31 11:05:22 -0500

    added task4 step2 to index.js file

---
+++ index.js
@@ -0,0 +1,41 @@
+let validFormFieldInput = (data) => {
+
+};
+
+    const taskName = document.querySelector('#formGroupExampleInput');
+    const task = taskName.value;
+
+    const taskDescription = document.querySelector('#formGroupExampleInput2');
+    const description = taskDescription.value;
+
+    const taskAssign = document.querySelector('#formGroupExampleInput3');
+    const assign = taskAssign.value;
+
+    const dueDate = document.querySelector('#newTaskDueDate');
+    const due = dueDate.value;
+
+    console.log("name: "+description);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
