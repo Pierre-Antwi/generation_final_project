e3dae2d4c6ffdcd51d336cf3b064c81051240ee7
Pierre Antwi <pierreantwi17@gmail.com>
2023-06-08 09:15:44 -0400

    Fixed Bugs

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 const taskManager = new TaskManager();
 
 
-let validFormFieldInput = (data) => {
+let formField = document.getElementById('formField');
+formField.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    alert('The form has been submitted')
 const taskName = document.querySelector('#taskNameInput');
 const taskInput = taskName.value;
-
+    if (typeof taskInput != 'string'){
+        taskInputLimit.style.display = 'block';
+    }
+  
 const taskDescription = document.querySelector('#taskDescriptionInput');
 const descriptionInput = taskDescription.value;
+ 
 
 const taskAssign = document.querySelector('#assign-to-Input');
 const assignToInput = taskAssign.value;
@@ -14,8 +21,13 @@ const assignToInput = taskAssign.value;
 const dueDate = document.querySelector('#taskDateInput');
 const dueDateInput = dueDate.value;
 
-console.log("name: "+ description);
-};
+taskManager.addTask(taskInput, descriptionInput, assignToInput, dueDateInput);
+
+console.log("taskName: "+ taskInput)
+console.log("description: "+ descriptionInput);
+console.log("assignee: "+ assignToInput)
+console.log("dueDate: "+ dueDateInput)
+})
 
 let taskInputLimit = document.getElementById('task-Name-alert');
 taskInputLimit.style.display = 'none';
@@ -26,48 +38,47 @@ descriptionLimit.style.display = 'none';
 let assignToLimit = document.getElementById('assign-To-alert');
 assignToLimit.style.display = 'none';
 
-let correctDateAlert = document.getElementById('due-Date-alert');
-correctDateAlert.style.display = 'none';
+let dateAlert = document.getElementById('due-Date-alert');
+dateAlert.style.display = 'none';
 
-const checkingValidInput = () => {
-    if (typeof taskInput === 'hello' && taskInput.length >= 10) {
-            if (taskInputLimit.style.display === 'none') {
-                taskInputLimit.style.display = 'block';
-            } else {
-                taskInputLimit.style.display = 'none';
-            }
-    }
-    if (typeof descriptionInput == 'hello' && description.length >= 50){
-            if (descriptionLimit.style.display === 'none') {
-                descriptionLimit.style.display = 'block';
-            } else {
-                descriptionLimit.style.display = 'none';
-            }
-    } 
-    if (typeof assignToInput === 'hello' && assignToInput.length >= 20){
-            if (assignToLimit.style.display === 'none') {
-                assignToLimit.style.display = 'block';
-            } else {
-                assignToLimit.style.display = 'none';
-            }
-    } else {
-        alert("Task had been added successfullly");
-    }
-}
+
+
+// const checkingValidInput = () => {
+//     if (typeof taskInput !== 'string' && taskInput.length >= 10) {
+//             if (taskInputLimit.style.display === 'none') {
+//                 taskInputLimit.style.display = 'block';
+//             } else {
+//                 taskInputLimit.style.display = 'none';
+//             }
+//     }
+//     if (typeof descriptionInput == 'string' && description.length >= 50){
+//             if (descriptionLimit.style.display === 'none') {
+//                 descriptionLimit.style.display = 'block';
+//             } else {
+//                 descriptionLimit.style.display = 'none';
+//             }
+//     } 
+//     if (typeof assignToInput === 'hello' && assignToInput.length >= 20){
+//             if (assignToLimit.style.display === 'none') {
+//                 assignToLimit.style.display = 'block';
+//             } else {
+//                 assignToLimit.style.display = 'none';
+//             }
+//     } else {
+//         alert("Task had been added successfullly");
+//     }
+// }
  
-let element = document.getElementById('addButton');
-//element.onclick = checkingValidInput;
-element.addEventListener("submit", () => {
-    if (checkingValidInput());
-    taskInput.addTask();
-    descriptionInput.addTask();
-    assignToInput.addTask();
 
-    render();
-})
+// element.onclick = ;
+// formField.addEventListener("click", () => {
+//     validFormFieldInput();
+//     taskInput.addTask();
+//     descriptionInput.addTask();
+//     assignToInput.addTask();
 
-
-
+//     taskManager.render();
+// })
 
 
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,15 +1,14 @@
-const createTaskHtml = (taskInput, descriptionInput, assignToInput, dueDate, status) =>{
-    const html = `
+const createTaskHtml = (name, description, assignTo, dueDate, status) =>{
+     `
     <div class="card" style="width: 25rem; height: 15rem">
     <div class="card-body">
-      <h5 class="task-name"><strong>Task Name : </strong> ${taskInput}}</h5><br>
-      <p class="task-description"><strong>Description : </strong> ${descriptionInput} </p><br>
-      <h6 class="assign-to"><strong>Assigned To:</strong> ${assignToInput}</h6><br>
+      <h5 class="task-name"><strong>Task Name : </strong> ${name}}</h5><br>
+      <p class="task-description"><strong>Description : </strong> ${description} </p><br>
+      <h6 class="assign-to"><strong>Assigned To:</strong> ${assignTo}</h6><br>
       <h6 class="due-date"><strong> Due Date : </strong> ${dueDate} </h6>
     </div>
   </div>`
 
-  return html;
 }
 
 class TaskManager  {
@@ -17,36 +16,40 @@ class TaskManager  {
         this.currentId = currentId;
         this.tasks = [];
     }
-    addTask (currentId, taskInput, descriptionInput, assignToInput, dueDateInput, status) {
-    this.currentId++;
-       return {
+    addTask (taskInput, descriptionInput, assignToInput, dueDateInput) {
+       const task =  {
         id : this.currentId++,
-        name : this.tasks[0],
-        description : this.tasks[1],
-        assignedTo : this.tasks[2],
-        dueDate : this.tasks[3],
-        status : this.tasks[4]
+        name : taskInput,
+        description : descriptionInput,
+        assignedTo : assignToInput,
+        dueDate : dueDateInput,
+        status : 'TODO'
         }
+
+        console.log(task);
     }
     render () {
         let taskHtmlList = [];
         let currenttask = this.tasks;
-        for (let i=0; i<=taskHtmlList.length, i++;){
+        for (let i=0; i<this.tasks.length; i++) {
+          const task = this.tasks[i]
           let date = new Date(dueDate);
           let formattedDate = date.toString;
-          let taskHtml = createTaskHtml(currenttask, description, assignTo, formattedDate);
+          let taskHtml = createTaskHtml(task.id, task.description, task.assignTo, task.dueDate, task.status);
           taskHtmlList.push(taskHtml);
         }
-        let tasksHtml = taskHtmlList.join('/n');
-        document.getElementById('Pro1').innerHTML = tasksHtml; 
-        document.getElementById('Pro2').innerHTML = tasksHtml;
-        document.getElementById('Pro3').innerHTML = tasksHtml;
-        document.getElementById('Pro4').innerHTML = tasksHtml;
-        document.getElementById('Per1').innerHTML = tasksHtml;
-        document.getElementById('Per2').innerHTML = tasksHtml;
-        document.getElementById('Per3').innerHTML = tasksHtml;
-        document.getElementById('Per4').innerHTML = tasksHtml;
-    }
+       let tasksHtml = taskHtmlList.join('/n');
+       const taskList = document.querySelector('#Pro1')
+       taskList.innerHTML = tasksHtml;
+    //     document.getElementById('Pro1').innerHTML = tasksHtml; 
+    //     document.getElementById('Pro2').innerHTML = tasksHtml;
+    //     document.getElementById('Pro3').innerHTML = tasksHtml;
+    //     document.getElementById('Pro4').innerHTML = tasksHtml;
+    //     document.getElementById('Per1').innerHTML = tasksHtml;
+    //     document.getElementById('Per2').innerHTML = tasksHtml;
+    //     document.getElementById('Per3').innerHTML = tasksHtml;
+    //     document.getElementById('Per4').innerHTML = tasksHtml;
+     }
 
 }    
 
