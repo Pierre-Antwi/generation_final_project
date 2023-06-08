6f862ea660ebe1a90c90098f65e0118da6808684
Pierre Antwi <pierreantwi17@gmail.com>
2023-06-08 15:30:28 -0400

    Task card display but overlap

--- js/index.js
+++ js/index.js
@@ -1,84 +1,32 @@
 const taskManager = new TaskManager();
 
-
 let formField = document.getElementById('formField');
-formField.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    alert('The form has been submitted')
-const taskName = document.querySelector('#taskNameInput');
-const taskInput = taskName.value;
-    if (typeof taskInput != 'string'){
-        taskInputLimit.style.display = 'block';
-    }
-  
-const taskDescription = document.querySelector('#taskDescriptionInput');
-const descriptionInput = taskDescription.value;
- 
+formField.addEventListener('submit', (e)=> {
+    e.preventDefault();    
+    const taskName = document.querySelector('#taskNameInput');
+    const taskDescription = document.querySelector('#taskDescriptionInput');
+    const taskAssign = document.querySelector('#assign-to-Input');
+    const dueDate = document.querySelector('#taskDateInput');
 
-const taskAssign = document.querySelector('#assign-to-Input');
-const assignToInput = taskAssign.value;
+    if(taskName.value === '') {
+        alert('Please enter a task name')
+    } else if (taskDescription.value === '') {
+        alert('Please enter a task description')
+    } else if(taskAssign.value === '') {
+        alert('Please enter a task assigned to')
+    } else if (dueDate.value === '') {
+        alert('Please enter a due date')
+    } 
 
-const dueDate = document.querySelector('#taskDateInput');
-const dueDateInput = dueDate.value;
+    const taskInput = taskName.value;
+    const descriptionInput = taskDescription.value;
+    const assignToInput = taskAssign.value;
+    const dueDateInput = dueDate.value;
 
 taskManager.addTask(taskInput, descriptionInput, assignToInput, dueDateInput);
-
-console.log("taskName: "+ taskInput)
-console.log("description: "+ descriptionInput);
-console.log("assignee: "+ assignToInput)
-console.log("dueDate: "+ dueDateInput)
-})
-
-let taskInputLimit = document.getElementById('task-Name-alert');
-taskInputLimit.style.display = 'none';
-
-let descriptionLimit = document.getElementById('description-alert');
-descriptionLimit.style.display = 'none';
-
-let assignToLimit = document.getElementById('assign-To-alert');
-assignToLimit.style.display = 'none';
-
-let dateAlert = document.getElementById('due-Date-alert');
-dateAlert.style.display = 'none';
-
-
-
-// const checkingValidInput = () => {
-//     if (typeof taskInput !== 'string' && taskInput.length >= 10) {
-//             if (taskInputLimit.style.display === 'none') {
-//                 taskInputLimit.style.display = 'block';
-//             } else {
-//                 taskInputLimit.style.display = 'none';
-//             }
-//     }
-//     if (typeof descriptionInput == 'string' && description.length >= 50){
-//             if (descriptionLimit.style.display === 'none') {
-//                 descriptionLimit.style.display = 'block';
-//             } else {
-//                 descriptionLimit.style.display = 'none';
-//             }
-//     } 
-//     if (typeof assignToInput === 'hello' && assignToInput.length >= 20){
-//             if (assignToLimit.style.display === 'none') {
-//                 assignToLimit.style.display = 'block';
-//             } else {
-//                 assignToLimit.style.display = 'none';
-//             }
-//     } else {
-//         alert("Task had been added successfullly");
-//     }
-// }
- 
-
-// element.onclick = ;
-// formField.addEventListener("click", () => {
-//     validFormFieldInput();
-//     taskInput.addTask();
-//     descriptionInput.addTask();
-//     assignToInput.addTask();
-
-//     taskManager.render();
-// })
+taskManager.render();
+console.log(taskManager.tasks);
+ })
 
 
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,14 +1,19 @@
 const createTaskHtml = (name, description, assignTo, dueDate, status) =>{
+  const html = 
      `
-    <div class="card" style="width: 25rem; height: 15rem">
-    <div class="card-body">
-      <h5 class="task-name"><strong>Task Name : </strong> ${name}}</h5><br>
-      <p class="task-description"><strong>Description : </strong> ${description} </p><br>
-      <h6 class="assign-to"><strong>Assigned To:</strong> ${assignTo}</h6><br>
-      <h6 class="due-date"><strong> Due Date : </strong> ${dueDate} </h6>
-    </div>
-  </div>`
+    <li>
+      <div class="card" style="width: 25rem; height: 15rem">
+      <div class="card-body">
+        <h5 class="task-name"><strong>Task Name : </strong> ${name}</h5><br>
+        <p class="task-description"><strong>Description : </strong> ${description} </p><br>
+        <h6 class="assign-to"><strong>Assigned To:</strong> ${assignTo}</h6><br>
+        <h6 class="due-date"><strong> Due Date : </strong> ${dueDate} </h6>
+        <h6 class="status><strong> Status : </strong> ${status}</h6>
+      </div>
+      </div>
+    </li>`
 
+  return html;
 }
 
 class TaskManager  {
@@ -25,31 +30,23 @@ class TaskManager  {
         dueDate : dueDateInput,
         status : 'TODO'
         }
-
+        this.tasks.push(task);
         console.log(task);
     }
     render () {
         let taskHtmlList = [];
-        let currenttask = this.tasks;
+        //let currentTask = this.tasks;
         for (let i=0; i<this.tasks.length; i++) {
-          const task = this.tasks[i]
-          let date = new Date(dueDate);
+          const task = this.tasks[i];
+          let date = new Date(task.dueDate);
           let formattedDate = date.toString;
-          let taskHtml = createTaskHtml(task.id, task.description, task.assignTo, task.dueDate, task.status);
+          let taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, task.dueDate, this.status);
           taskHtmlList.push(taskHtml);
         }
-       let tasksHtml = taskHtmlList.join('/n');
-       const taskList = document.querySelector('#Pro1')
+       let tasksHtml = taskHtmlList.join();
+       const taskList = document.querySelector('.list-group');
        taskList.innerHTML = tasksHtml;
-    //     document.getElementById('Pro1').innerHTML = tasksHtml; 
-    //     document.getElementById('Pro2').innerHTML = tasksHtml;
-    //     document.getElementById('Pro3').innerHTML = tasksHtml;
-    //     document.getElementById('Pro4').innerHTML = tasksHtml;
-    //     document.getElementById('Per1').innerHTML = tasksHtml;
-    //     document.getElementById('Per2').innerHTML = tasksHtml;
-    //     document.getElementById('Per3').innerHTML = tasksHtml;
-    //     document.getElementById('Per4').innerHTML = tasksHtml;
      }
 
-}    
+}; 
 
